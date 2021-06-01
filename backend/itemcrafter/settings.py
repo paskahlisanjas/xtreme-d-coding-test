8e724cc4e6e3787b6a75fb8ceb2545cf4ce0166c
Paskahlis Anjas <pjaswo1204@gmail.com>
2021-06-01 19:33:52 +0700

    BE: allow some CORS

--- backend/itemcrafter/settings.py
+++ backend/itemcrafter/settings.py
@@ -38,15 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'ingredient',
     'recipe',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
