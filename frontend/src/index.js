b3d0cdae1eba0838ed516f8450f24c1bd30b638e
Paskahlis Anjas <pjaswo1204@gmail.com>
2021-06-02 23:35:06 +0700

    FE: experiment with drag & drop

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,18 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import App from './App';
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
--- frontend/src/pages/WorkbenchPage.jsx
+++ frontend/src/pages/WorkbenchPage.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 import StandardLayout from '../components/templates/StandardLayout';
 
+import './styles.css';
+
 const WorkbenchPage = () => {
+  const [{ opacity }, dragRef] = useDrag(
+    () => ({
+      type: 'tile',
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1,
+      }),
+    }),
+    []
+  );
+
   return (
-    <StandardLayout>
-      Workbench page is applying atomic design principle!
+    <StandardLayout header="Workbench Arrangement">
+      <div className="grid-container">
+        {[...Array(16).keys()].map(() => (
+          <div className="grid-item" />
+        ))}
+      </div>
+      <div
+        ref={dragRef}
+        style={{ width: '64px', height: '64px', background: 'green' }}
+      >
+        drag me
+      </div>
     </StandardLayout>
   );
 };
