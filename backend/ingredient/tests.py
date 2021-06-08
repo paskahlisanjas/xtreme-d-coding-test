0cf9d146e126049cc372f26e54fa2c9da3252a5e
Paskahlis Anjas <pjaswo1204@gmail.com>
2021-06-08 21:34:32 +0700

    BE: Add unit test for ingredients view

--- backend/ingredient/tests.py
+++ backend/ingredient/tests.py
@@ -1,3 +1,6 @@
+import json
+
+from rest_framework.test import RequestsClient
 from django.test import TestCase
 from .models import Ingredient
 
@@ -12,7 +15,9 @@ class IngredientTest(TestCase):
             representation='G',
             illustration_url='https://www.glass-image.com/file.png')
 
+
     def test_attributes_matches(self):
+        """test object query, should return all records & matches all the attributes"""
         wood = Ingredient.objects.get(name='Wood')
         glass = Ingredient.objects.get(name='Glass')
         self.assertEqual(wood.name, 'Wood')
@@ -21,3 +26,15 @@ class IngredientTest(TestCase):
         self.assertEqual(glass.name, 'Glass')
         self.assertEqual(glass.representation, 'G')
         self.assertEqual(glass.illustration_url, 'https://www.glass-image.com/file.png')
+
+
+    def test_api_list_ingredients_response_length(self):
+        """test GET /api/ingredients/, should return all records"""
+        client = RequestsClient()
+        response = client.get('http://testserver/api/ingredients/')
+        self.assertEqual(response.status_code, 200)
+
+        decoded_content = bytes.decode(response.content)
+        data = json.loads(decoded_content)
+        self.assertEqual(len(data), 2)
+    
